cf1b97fd3939336e2f94ba5e69e62ddd0d1c3809
shadowed1 <itszachg@gmail.com>
2026-01-14 16:56:23 -0500

    Update elevate_installer.sh

--- elevate_installer.sh
+++ elevate_installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# ChromeOS Elevated Shell
+# Sucros Installer
 # shadowed1
 # Must be run as sudo logged in as chronos in VT-2
 
 mkdir -p /usr/local/bin
-curl -L https://raw.githubusercontent.com/shadowed1/elevate/main/elevate.sh -o /usr/local/bin/elevate
-curl -L https://raw.githubusercontent.com/shadowed1/elevate/main/elevate-daemon.sh -o /usr/local/bin/elevate-daemon
-curl -L https://raw.githubusercontent.com/shadowed1/elevate/main/elevate.conf -o /etc/init/elevate.conf
-chmod +x /usr/local/bin/elevate
-chmod +x /usr/local/bin/elevate-daemon
-rm -f /usr/local/elevate_installer
+curl -L https://raw.githubusercontent.com/shadowed1/sucros/main/sucros.sh -o /usr/local/bin/sucros
+curl -L https://raw.githubusercontent.com/shadowed1/sucros/main/sucros-daemon.sh -o /usr/local/bin/sucros-daemon
+curl -L https://raw.githubusercontent.com/shadowed1/sucros/main/sucros.conf -o /etc/init/sucros.conf
+chmod +x /usr/local/bin/sucros
+chmod +x /usr/local/bin/sucros-daemon
+rm -f /usr/local/sucros_installer
