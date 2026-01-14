d431698ac4acaf7f523387e385ad22e2b388d0f6
shadowed1 <itszachg@gmail.com>
2026-01-14 13:53:04 -0500

    Update elevate-daemon.sh

--- elevate-daemon.sh
+++ elevate-daemon.sh
@@ -2,11 +2,11 @@
 # ChromeOS Elevate Daemon
 # shadowed1
 
-FIFO="/tmp/elevate.fifo"
+FIFO="/usr/local/elevate/elevate.fifo"
 [[ -p "$FIFO" ]] || mkfifo "$FIFO"
 chmod 600 "$FIFO"
 
-echo "[elevate-daemon] Listening for commands on $FIFO"
+echo "[elevate-daemon] Listening on $FIFO"
 
 while true; do
     if read -r cmd < "$FIFO"; then
