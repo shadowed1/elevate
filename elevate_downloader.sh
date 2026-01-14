75df81a32a0412bc5a0ee34508eaaca0c65096fe
shadowed1 <itszachg@gmail.com>
2026-01-14 16:56:55 -0500

    Update elevate_downloader.sh

--- elevate_downloader.sh
+++ elevate_downloader.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# ChromeOS Elevated Shell
+# Sucros Downloader
 # shadowed1
-# Non sudo downloader
-curl -L https://raw.githubusercontent.com/shadowed1/elevate/main/elevate_installer.sh -o /home/chronos/elevate_installer
+
+curl -L https://raw.githubusercontent.com/shadowed1/sucros/main/sucros_installer.sh -o /home/chronos/sucros_installer
 echo
 echo "Run the commands listed below in VT-2 to continue the installer:"
 echo "ctrl-alt-refresh to open VT-2 (ctrl-alt-back to exit) - log in as chronos"
 echo ""
-echo "sudo mv /home/chronos/elevate_installer /usr/local/"
-echo "sudo chmod +x /usr/local/elevate_installer"
-echo "sudo /usr/local/elevate_installer"
+echo "sudo mv /home/chronos/sucros_installer /usr/local/"
+echo "sudo chmod +x /usr/local/sucros_installer"
+echo "sudo /usr/local/sucros_installer"
 echo
