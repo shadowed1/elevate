ebf0972498bbd31c9723e2cdafd23246203cf6dc
shadowed1 <itszachg@gmail.com>
2026-02-06 21:14:27 -0500

    Create sucrose_reinstall.sh

---
+++ bin/sucrose_reinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L https://raw.githubusercontent.com/shadowed1/sucrose/main/bin/sucrose_installer.sh -o /home/chronos/user/sucrose_installer
+sudo bash /home/chronos/user/sucrose_installer
